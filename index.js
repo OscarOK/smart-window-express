91fb26873f6286b4a124c5278bded80643f80004
Erick Blanco <erick.jassi.bs@gmail.com>
2020-12-01 17:43:44 -0700

    no me falles heroku, confio en ti

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const socketio = require('socket.io');
+
+const PORT = 3000 || process.env.PORT;
+
+const app = express();
+app.use(express.static(__dirname + '/public'));
+
+const server = app.listen(PORT);
+const io = socketio(server, {
+    cors: {
+        origin: '*',
+    }
+});
+
+io.on('connection', (socket) => {
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+    });
+
+    socket.on('caregiver', (data) => {
+        socket.emit('elderly', data);
+    });
+
+    socket.on('elderly', (data) => {
+        socket.emit('caregiver', data);
+    });
+
+    socket.on('data', (data) => {
+        socket.broadcast.emit('data', data);
+    })
+});
